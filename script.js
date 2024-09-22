14075e3a3ace0ea57a0d8a416f5164eaf10f7d13
GermanBalaguera954 <germanbalaguera954@gmail.com>
2024-09-22 09:25:12 -0500

    second commit

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ const openCardBtn = document.getElementById('openCardBtn');
 
 openCardBtn.addEventListener('click', () => {
     card.classList.toggle('is-flipped');
+
+    if (card.classList.contains('is-flipped')) {
+        setTimeout(() => {
+            card.classList.remove('is-flipped');
+        }, 10000);
+    }
 });
+
